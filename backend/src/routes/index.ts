73d29268d0af27f8f8b493f671efada16ba17cc0
Cairo <cairolopes011@gmail.com>
2024-06-25 19:56:30 -0300

    commit inicial

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+
+
+const appRouter = express();
+
+export default appRouter;
---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TFASA6n1I8bzh4PhsdKZeaPoyZTjWQ5mFND39hKZoOI=\"\n}"
